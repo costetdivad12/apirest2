3ab31354424d36f00ffd54f6601db9b4389c2aca
costetdivad12 <david.costet@iebem.edu.mx>
2022-06-23 16:01:54 -0500

    first commit

---
+++ src/test/java/mx/edu/iebem/api2/Api2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.iebem.api2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
